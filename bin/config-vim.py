a540720145aefa385dbe1d7b7163008e37279c3a
Farzad Ghanei <farzad.ghanei@gmail.com>
2017-10-03 11:40:09 +0200

    config-vim.py installs vim-plug plugins, prompts for vundle installs

--- bin/config-vim.py
+++ bin/config-vim.py
@@ -14,12 +14,14 @@ def ask_yes_no(msg, default_accept=True):
         return ans != 'n'
     return ans == 'y'
 
+
 def configure_vim(target, ask=True):
     if not ask_yes_no("Configure vim on: '{}'?".format(target)):
         return False
     copy_dot_vim(dotfiles, target, ask)
-    init_vundle(target, ask)
-    install_vim_plugins_with_vundle(target, ask)
+    if init_vundle(target, ask):
+        install_vim_plugins_with_vundle(target, ask)
+    install_vim_plugins_with_plug(target, ask)
 
 def copy_dot_vim(src, dst, ask=True):
     src_vimrc = os.path.join(src, '.vimrc')
@@ -40,7 +42,10 @@ def copy_dot_vim(src, dst, ask=True):
         shutil.rmtree(dst_dot_vim)
     shutil.copytree(src_dot_vim, dst_dot_vim)
 
+
 def init_vundle(root, ask=True):
+    if ask and not ask_yes_no("Install Vundle (required to install most plugins)?"):
+        return False
     dot_vim = os.path.join(root, '.vim')
     if not os.path.exists(dot_vim):
         os.mkdir(dot_vim)
@@ -50,15 +55,30 @@ def init_vundle(root, ask=True):
     vundle_path = os.path.join(bundle_path, 'Vundle.vim')
     shutil.rmtree(vundle_path, True)
     subprocess.call(['git', 'clone', 'https://github.com/gmarik/Vundle.vim.git', vundle_path])
+    return True
+
 
 def install_vim_plugins_with_vundle(target, ask):
-    vimrc = os.path.join(target, '.vimrc')
-    subprocess.call(['vim', '-u', vimrc, '+PluginInstall'])
+    if ask and ask_yes_no("Install Vundle plugins?"):
+        vimrc = os.path.join(target, '.vimrc')
+        subprocess.call(['vim', '-u', vimrc, '+PluginInstall'])
+
+
+def install_vim_plugins_with_plug(target, ask):
+    """
+    Plugin management tool, used for FZF vim plugin
+    https://github.com/junegunn/vim-plug
+    """
+    if ask and ask_yes_no("Install vim-plug plugins?"):
+        vimrc = os.path.join(target, '.vimrc')
+        subprocess.call(['vim', '-u', vimrc, '+PlugInstall'])
+
 
 def main():
     home_path = os.path.expanduser('~')
     target = sys.argv[1] if len(sys.argv) > 1 else home_path
     return configure_vim(target) and 1 or 0
+
 
 if __name__ == '__main__':
     sys.exit(main())
